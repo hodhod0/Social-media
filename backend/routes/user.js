09cc986fc71c2c83d304b48992dfc9b0b0f1ec6a
hodod <alameddinehodhod@gmail.com>
2022-09-08 11:06:09 +0300

    delete and get a user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,7 +4,7 @@ const User = require("../models/userModel");
 
 //UPDATE USER
 router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id || req.user.isAdmin) {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
     if (req.body.password) {
       try {
         const salt = await bcrypt.genSalt(10);
@@ -25,7 +25,32 @@ router.put("/:id", async (req, res) => {
   }
 });
 //DELETE USER
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+     
+      try{
+          const user = await User.findByIdAndDelete(req.params.id)
+          res.status(200).json("account has been deleted")
+      }catch(err){
+          return res.status(403).json("You can only update your account");
+  
+      }
+    } else {
+      return res.status(403).json("You can only delete your account");
+    }
+  });
 //GET A USER
+router.get("/:id", async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        const {password,updatedAt, ...other} = user._doc
+        res.status(200).json(other)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error)
+    }
+
+})
 //FOLLOW A USER
 //UNFOLLOW A USER
 
